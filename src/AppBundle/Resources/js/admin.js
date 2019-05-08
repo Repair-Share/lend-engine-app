1b0cc47d6a52006b1e8244da66bc36413e29c431
Chris Tanner <chris@lend-engine.com>
2019-05-08 08:22:29 +0100

    Fix bug with barcode scanning

--- src/AppBundle/Resources/js/admin.js
+++ src/AppBundle/Resources/js/admin.js
@@ -214,13 +214,15 @@ function waitButton(obj) {
     obj.removeClass('bg-green').addClass('btn-default').attr('disabled', true).before('<img src="/images/ajax-loader.gif" style="padding-right: 10px">');
 }
 
-// ALSO IN member_site.js
 var barcode = '';
 $(document).keypress(function(e){
-    setTimeout(resetBarcode, 1000);
-    if (e.which == 13 && barcode != '') {
-        document.location.href = '/admin/item/list?search='+barcode;
-        console.log("Found barcode, let's go!");
+    setTimeout(resetBarcode, 600);
+    if (e.which == 13 && barcode.length > 3) {
+        console.log("Code entered over 3 char : "+barcode);
+        if (!isNaN(barcode)) {
+            document.location.href = '/admin/item/list?search='+barcode;
+            console.log("Found barcode, let's go!");
+        }
     }
     barcode = barcode + e.key;
 });
