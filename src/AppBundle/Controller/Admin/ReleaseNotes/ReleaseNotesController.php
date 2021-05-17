99f852831eb0ad1df50a785c626fb27819cdc5d2
andortamas <andordev@gmail.com>
2021-05-17 18:42:16 +0200

    Updated release notes

--- src/AppBundle/Controller/Admin/ReleaseNotes/ReleaseNotesController.php
+++ src/AppBundle/Controller/Admin/ReleaseNotes/ReleaseNotesController.php
@@ -18,17 +18,18 @@ class ReleaseNotesController extends Controller
         $gitHubIssueLink = 'https://github.com/lend-engine/lend-engine-app/issues/';
 
         $bugfixes = [
+            '105' => 'Events don\'t adhere to the maximum attendees',
             '110' => 'Add Catalan language',
             '109' => 'Update language Slovenščina',
             '101' => 'Exception: DateTime::__construct(): (Rollbar 101)',
-            '99' => 'Exception: DateTime::__construct(): (Rollbar 98)',
-            '97' => 'Add a release notes page to admin',
-            '96' => 'Incorrect hours being sent in emails (with unit tests)'
+            '99'  => 'Exception: DateTime::__construct(): (Rollbar 98)',
+            '97'  => 'Add a release notes page to admin',
+            '96'  => 'Incorrect hours being sent in emails (with unit tests)'
         ];
 
         return $this->render('release_notes/notes.html.twig', [
-            'bugfixes' => $bugfixes,
-            'issueLink'=> $gitHubIssueLink
+            'bugfixes'  => $bugfixes,
+            'issueLink' => $gitHubIssueLink
         ]);
     }
 }
