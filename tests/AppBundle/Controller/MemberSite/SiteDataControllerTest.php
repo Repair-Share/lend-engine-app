e00deb0cf1fb2a705c7852968628d4fe42ffa801
andortamas <andordev@gmail.com>
2021-05-31 12:27:51 +0200

    Unit test for overlapping opening hours #116

--- tests/AppBundle/Controller/MemberSite/SiteDataControllerTest.php
+++ tests/AppBundle/Controller/MemberSite/SiteDataControllerTest.php
@@ -71,4 +71,64 @@ class SiteDataControllerTest extends AuthenticatedControllerTest
         $this->assertNotContains($wednesday->format('Y-m-d 09:00:00'), $responseContent); // Wednesday is closed
 
     }
+
+    public function testOverlappingOpeningHours()
+    {
+        // Add a new site
+        $siteID = $this->helpers->createSite($this->client);
+
+        // Set up this week's Monday
+        $firstDayOfWeek = 1; // Monday
+
+        $monday = new \DateTime();
+
+        $difference = ($firstDayOfWeek - $monday->format('N'));
+        $monday->modify("$difference days");
+
+        $tuesday = clone $monday;
+        $tuesday->modify('1 day');
+
+        // Add custom hours / holiday to the site
+
+        // Monday is closed
+        $this->helpers->addSiteOpeningHours(
+            $this->client,
+            $siteID,
+            $monday,
+            '0900',
+            '1700',
+            false
+        );
+
+        // Tuesday is closed
+        $this->helpers->addSiteOpeningHours(
+            $this->client,
+            $siteID,
+            $tuesday,
+            '0900',
+            '1700',
+            false
+        );
+
+        // Create an item
+        $itemID = $this->helpers->createItem($this->client);
+
+        // Check site data
+        $now = new \DateTime();
+
+        $uri = '/site-data?itemId=' . $itemID
+               . '&start=' . urlencode($now->modify('-2 weeks')->format('Y-m-d'))
+               . '&end=' . urlencode($now->modify('+4 weeks')->format('Y-m-d'));
+
+        $this->client->request('GET', $uri);
+        $response = $this->client->getResponse();
+
+        $this->assertSame(200, $response->getStatusCode());
+
+        $responseContent = $response->getContent();
+
+        // Tuesday should be opened as other site should be opening
+        $this->assertContains($tuesday->format('Y-m-d 09:00:00'), $responseContent);
+
+    }
 }
