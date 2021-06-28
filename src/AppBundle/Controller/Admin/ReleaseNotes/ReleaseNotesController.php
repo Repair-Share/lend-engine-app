0bc936900342f1396c9b6c6524a7b9f52177a111
andortamas <andordev@gmail.com>
2021-06-28 13:29:45 +0200

    Updated the ReleaseNotesController

--- src/AppBundle/Controller/Admin/ReleaseNotes/ReleaseNotesController.php
+++ src/AppBundle/Controller/Admin/ReleaseNotes/ReleaseNotesController.php
@@ -18,6 +18,7 @@ class ReleaseNotesController extends Controller
         $gitHubIssueLink = 'https://github.com/lend-engine/lend-engine-app/issues/';
 
         $bugfixes = [
+            '111' => 'Update Welsh language after translation',
             '128' => 'Loans do not adhere to conditions set out in opening hours and will set to opening time instead of closing time.',
             '127' => 'Booking item allows return on a closed day',
             '123' => 'As a user you can check-in on an event that is not self serve "book now" is not available',
