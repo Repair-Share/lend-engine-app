556d1c493c2373ff309ca5dc1a045b88b126ed32
Andor Tamas <andordev@gmail.com>
2021-12-13 15:18:06 +0100

    Adding search_terms into the settings table #168

--- app/DoctrineMigrations/Version20211223141517.php
+++ app/DoctrineMigrations/Version20211223141517.php
@@ -15,7 +15,7 @@ final class Version20211223141517 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql("INSERT INTO `setting` VALUES ('search_terms','or');");
+        $this->addSql("INSERT INTO `setting` VALUES ('search_terms','1');");
     }
 
     public function down(Schema $schema) : void
