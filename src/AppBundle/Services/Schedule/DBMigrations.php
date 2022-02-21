b10eb5146ce7cbd104e5b5faf94fe988866d3137
Andor Tamas <andordev@gmail.com>
2022-02-21 18:01:44 +0100

    Revert "Removed the unique loan_row_id #175"
    This reverts commit 27217e8cd5f601005fbf505173021681f24696b6.

--- src/AppBundle/Services/Schedule/DBMigrations.php
+++ src/AppBundle/Services/Schedule/DBMigrations.php
@@ -70,7 +70,7 @@ class DBMigrations
     public function migrate($server)
     {
         // Help https://symfony.com/doc/3.4/console.html
-        $expectedMigrationVersion = '20220221152719';
+        $expectedMigrationVersion = '20211223141517';
 
         $resultString = '';
 
@@ -143,7 +143,6 @@ class DBMigrations
                 'user'     => $username,
                 'password' => $password
             ];
-
             $conn   = new Connection($params, $driver);
             $config = new Configuration($conn);
 
