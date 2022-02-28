b2f41a0091b58c23312eab48bf30bd87dde7c282
Andor Tamas <andordev@gmail.com>
2022-02-28 17:29:48 +0100

    Removed the unique loan_row_id #175

--- src/AppBundle/Services/Schedule/DBMigrations.php
+++ src/AppBundle/Services/Schedule/DBMigrations.php
@@ -70,7 +70,7 @@ class DBMigrations
     public function migrate($server)
     {
         // Help https://symfony.com/doc/3.4/console.html
-        $expectedMigrationVersion = '20211223141517';
+        $expectedMigrationVersion = '20220221152719';
 
         $resultString = '';
 
