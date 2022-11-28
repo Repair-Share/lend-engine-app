27a770bab58539df7327db505c4053573d7bc09d
Andor Tamas <andordev@gmail.com>
2022-11-28 17:54:54 +0100

    Unit test for #220: bugfix

--- src/AppBundle/Controller/Settings/OpeningHoursController.php
+++ src/AppBundle/Controller/Settings/OpeningHoursController.php
@@ -6,6 +6,7 @@ use AppBundle\Entity\Event;
 use AppBundle\Entity\Setting;
 use AppBundle\Entity\Site;
 use AppBundle\Form\Type\Settings\OpeningHoursType;
+use AppBundle\Helpers\UnitTestHelper;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
