fdd41ddc36c4f7ad9fb508829bc1436db6e8fdd4
Andor Tamas <andordev@gmail.com>
2023-01-30 18:51:09 +0100

    Unit test for #195

--- tests/AppBundle/Controller/Loan/LoanCheckInControllerTest.php
+++ tests/AppBundle/Controller/Loan/LoanCheckInControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\AppBundle\Controller\Loan;
 
+use AppBundle\Entity\PaymentMethod;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Tests\AppBundle\Controller\AuthenticatedControllerTest;
 
@@ -54,6 +55,70 @@ class LoanCheckInControllerTest extends AuthenticatedControllerTest
         $this->assertContains("Check in note text", $crawler->html()); // note added
         $this->assertContains("Check-in fee for ".$itemName, $crawler->html()); // fee added
         $this->assertContains("1.29", $crawler->html()); // fee added
+    }
+
+    public function testLoanCheckInWithRefund()
+    {
+        // Create a new item with deposit amount
+        $itemName = "Check in test item with deposit " . rand();
+        $itemId   = $this->helpers->createItem($this->client, $itemName, ['depositAmount' => 10]);
+
+        // Add a loan
+        $loanId = $this->helpers->createLoan($this->client, 2, [$itemId]);
+
+        // Go to it
+        $crawler = $this->client->request('GET', '/loan/' . $loanId);
+        $this->assertContains("loan/{$loanId}", $crawler->html()); // We are on the loan page
+
+        // Check it out
+        $form = $crawler->filter('form[name="loan_check_out"]')->form(array(
+            'loan_check_out[paymentMethod]' => 1,
+            'loan_check_out[paymentAmount]' => 10.00,
+        ), 'POST');
+        $this->client->submit($form);
+        $this->assertTrue($this->client->getResponse() instanceof RedirectResponse);
+        $crawler = $this->client->followRedirect();
+
+        $rowId = $crawler->filter('.btn_checkin')->first()->attr('data-loan-row-id');
+
+        $crawler = $this->client->request('GET', '/loan-row/' . $rowId . '/check-in/');
+        $this->assertContains('Check in "' . $itemName . '"', $crawler->html());
+        $this->assertContains('A deposit of <strong>£10.00</strong> was taken for this item.', $crawler->html());
+
+        // Open the refund modal
+        $paymentId = $crawler->filter('.refund-button')->attr('data-payment-id');
+        $crawler   = $this->client->request(
+            'GET',
+            '/admin/refund?id=' . $paymentId . '&amount=10.00&goToCheckInItem=' . $rowId
+        );
+
+        $this->assertContains('This screen is used when you are giving money back to a member.', $crawler->html());
+        $this->assertContains('Create Debit to LE account', $crawler->html());
+
+        $form = $crawler->filter('form[name="refund"]')->form([
+            'refund[amount]'       => 10,
+            'refund[note]'         => 'Debit LE account test',
+            'refund[debitAccount]' => false
+        ], 'POST');
+
+        $this->client->submit($form);
+        $this->assertTrue($this->client->getResponse() instanceof RedirectResponse);
+        $crawler = $this->client->followRedirect();
+
+        $this->assertContains('The deposit has been refunded.', $crawler->html());
+
+        // Check the payments
+        $crawler = $this->client->request(
+            'GET',
+            '/member/payments'
+        );
+
+        // Not perfect, but better than nothing
+        $this->assertContains(
+            PaymentMethod::PAYMENT_METHOD_DEBIT_ACCOUNT,
+            $crawler->html()
+        );
+
     }
 
 }
