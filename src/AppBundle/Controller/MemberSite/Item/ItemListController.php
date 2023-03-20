1bfbbf82eae6fa439b227eeba47beb5c167fd7b9
Andor Tamas <andordev@gmail.com>
2023-03-20 17:02:47 +0100

    Live debug: remove

--- src/AppBundle/Controller/MemberSite/Item/ItemListController.php
+++ src/AppBundle/Controller/MemberSite/Item/ItemListController.php
@@ -28,17 +28,6 @@ class ItemListController extends Controller
             return $this->redirectToRoute('home');
         }
 
-        echo '<pre style="display: none">';
-
-        echo print_r([
-            @$_SERVER['HTTP_HOST'],
-            @$_SERVER['HTTP_X_FORWARDED_PORT'],
-            @$_SERVER['HTTP_X_FORWARDED_PROTO'],
-            @$_SERVER['HTTP_X_FORWARDED_FOR']
-        ]);
-
-        echo '</pre>';
-
         $pageSize = 50;
         $pageTitle = '';
 
